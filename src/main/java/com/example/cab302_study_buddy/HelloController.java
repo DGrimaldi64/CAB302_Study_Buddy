891a9334552079f7ae3074bb7ad338b6a451dc4b
DGrimaldi64 <you@example.com>
2024-04-24 16:57:03 +1000

    Initial Commit

---
+++ src/main/java/com/example/cab302_study_buddy/HelloController.java
@@ -0,0 +1,49 @@
+package com.example.cab302_study_buddy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onTasksClick() {
+        // change scene to Tasks
+    }
+
+    @FXML
+    protected void onCalendarClick() {
+        // change scene to Calendar
+    }
+
+    @FXML
+    protected void onTimerClick() {
+        // change scene to Timer
+    }
+
+    @FXML
+    protected void onRewardslick() {
+        // change scene to Rewards
+    }
+
+    @FXML
+    protected void onStudyHelperClick() {
+        // change scene to Study Helper
+    }
+
+    @FXML
+    protected void onAnalyticsClick() {
+        // change scene to Analytics
+    }
+
+    @FXML
+    protected void onAccessibilityClick() {
+        // change scene to Accessibility
+    }
+
+    @FXML
+    protected void onSettingsClick() {
+        // change scene to Settings
+    }
+}
