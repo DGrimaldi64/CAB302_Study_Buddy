60a9209a5900253e559f54f4aaa8a8ed05a34f66
Yash Akoliya <yashakoliya45@gmail.com>
2024-05-13 15:27:41 +1000

    Updated Calendar

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.cab302_study_buddy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.cab302_study_buddy to javafx.fxml;
-    exports com.example.cab302_study_buddy;
-}
